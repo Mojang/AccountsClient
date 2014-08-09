1f5b297f4eaf1dd3e53812bd8fbcb0bbd7e57c0e
Mazen K <email@mazenmc.io>
2014-08-09 14:31:21 +0400

    Mavenize!

--- src/main/java/com/mojang/api/profiles/HttpProfileRepository.java
+++ src/main/java/com/mojang/api/profiles/HttpProfileRepository.java
@@ -11,7 +11,6 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 
 public class HttpProfileRepository implements ProfileRepository {
