e39428474cb644227e1d06bff51575a04f521732
computerwizjared <computerwizjared@hotmail.com>
2015-05-27 17:38:16 -0500

    Update to Spigot 1.8 to allow PrisonRankup to be loaded

--- src/main/java/com/mojang/api/profiles/HttpProfileRepository.java
+++ src/main/java/com/mojang/api/profiles/HttpProfileRepository.java
@@ -1,17 +1,12 @@
 package com.mojang.api.profiles;
 
-import com.mojang.api.http.BasicHttpClient;
-import com.mojang.api.http.HttpBody;
-import com.mojang.api.http.HttpClient;
-import com.mojang.api.http.HttpHeader;
-import net.minecraft.util.com.google.gson.Gson;
+import com.google.gson.Gson;
+import com.mojang.api.http.*;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class HttpProfileRepository implements ProfileRepository {
 
